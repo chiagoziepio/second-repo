ae111b049717f2c0c8b931fc32d62e1d372cbbbb
chiagozie pio <nduagubapio@gmail.com>
2023-09-02 19:20:37 +0100

    version 2

--- begin.js
+++ begin.js
@@ -1,3 +1,3 @@
-console.log("version 1");
+console.log("version 2");
 console.log("begin");
 console.log("checking");
--- srcs/start.js
+++ srcs/start.js
@@ -1,3 +1,3 @@
-console.log("version 1");
+console.log("version 2");
 console.log("start");
 console.log("checking");
